170f8e6f75a9c69c35e560e0ac6cb86d4e6bc9f5
Hariom Subudhi <hariom24subudhi@gmail.com>
2024-04-06 13:57:21 +0530

    added mail functionality

--- script.js
+++ script.js
@@ -23,3 +23,30 @@ menuIcon.onclick=()=>{
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+
+
+function Sendmail(){
+    var params = {
+        user_name: document.getElementById("user_name").value,
+        user_mailid: document.getElementById("user_mailid").value,
+        user_pno: document.getElementById("user_pno").value,
+        user_sub: document.getElementById("user_sub").value,
+        user_message: document.getElementById("user_message").value,
+    };
+    const serviceId="service_h35dlhr";
+    const templateid= "template_2o3eo1c";
+
+    emailjs
+        .send(serviceId, templateid, params)
+        .then((res) => {
+            document.getElementById("user_name").value= "";
+            document.getElementById("user_mailid").value= "";
+            document.getElementById("user_message").value= "";
+            document.getElementById("user_pno").value= "";
+            document.getElementById("user_sub").value= "";
+            alert("Your Message has been sent successfully");
+        })
+        .catch((err) => console.log(err));
+}
+
